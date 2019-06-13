58ba8e6515d21cd4c11691f94d4c75307cb2abd5
Firdows <ahamad.jedu@gmail.com>
2019-06-13 09:48:20 +0000

    New save to git

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1566600456');
+define('LINE_MESSAGE_CHANNEL_SECRET','e891bb1735f26b37efc4171c051db73d');
+define('LINE_MESSAGE_ACCESS_TOKEN','vUEmNN/+z1iCBynci8qOg5scBjApRBcGALpVCSdrB5K1fWS+du5rReY8TNzf/aioXcd9bVk2v+oNFoWXTGjiysdqo27cVg8YwpffsElsB3+Sa8bdty5Svue1KVqaylt5uZ/IyZXvvYbJrMxso3kv9AdB04t89/1O/w1cDnyilFU=');
+?>
---
+++ test.php
@@ -0,0 +1,67 @@
+<?php
+// การตั้งเกี่ยวกับ bot
+require_once 'config.php';
+ 
+// กรณีมีการเชื่อมต่อกับฐานข้อมูล
+//require_once("dbconnect.php");
+ 
+///////////// ส่วนของการเรียกใช้งาน class ผ่าน namespace
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+//use LINE\LINEBot\Event;
+//use LINE\LINEBot\Event\BaseEvent;
+//use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\MessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+use LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+use LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+use LINE\LINEBot\ImagemapActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder ;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
+use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
+ 
+ 
+$httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+$bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
+ 
+// userId 
+$userId[] = 'U9bf5f07267af5e44e0536e0b53189b2e';#one
+//$userId[] = 'U4f6ec87da31b2d6151760469328d3340';#wee
+//$userId[] = 'U272d6cb945650f54ababb9bd67c99055';#Joke
+//$userId[] = 'U73f5fb2dcd0d10ba95aad78f2f7abe44';#ake
+//$userId[] = 'U7b981af0dec961220baad3113b21e8a6';#Jean
+// ทดสอบส่ง push ข้อความอย่างง่าย
+$textPushMessage = 'หวัดดีบอลโลก';                
+$textPushMessage = 'มีปัญหาแจ้งเข้ามาใหม่ 1 รายการ';                
+$messageData = new TextMessageBuilder($textPushMessage);        
+             
+$response = $bot->multicast($userId,$messageData);             
+//$response = $bot->pushMessage($userId,$messageData);
+if ($response->isSucceeded()) {
+    echo 'Succeeded!';
+    return;
+}
+ 
+// Failed
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+?>
